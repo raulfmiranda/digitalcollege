2a1138626e2390aa45e48a089ef582f562486e58
Raul <raulfmiranda@gmail.com>
2022-09-19 19:43:13 -0300

    fetch estados e municipios

--- modulo-js/ibge/eventos.js
+++ modulo-js/ibge/eventos.js
@@ -1,9 +1,35 @@
 const URL_REGIOES = 'https://servicodados.ibge.gov.br/api/v1/localidades/regioes';
+const urlEstados = (id) => `https://servicodados.ibge.gov.br/api/v1/localidades/regioes/${id}/estados`;
+const urlMunicipios = (id) => `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${id}/municipios`;
 
 fetch(URL_REGIOES)
     .then(response => response.json())
     .then(data => {
         data.map(cadaResult => {
-            regiao.innerHTML += `<option>${cadaResult.nome}</option>`
+            regiao.innerHTML += `<option value="${cadaResult.id}">${cadaResult.nome}</option>`
         });
     });
+
+regiao.addEventListener('change', () => {
+    estado.innerHTML = '<option>-- Selecione --</option>';
+
+    fetch(urlEstados(regiao.value))
+        .then(response => response.json())
+        .then(dados => {
+            dados.map(cadaResult => {
+                estado.innerHTML += `<option value="${cadaResult.id}">${cadaResult.nome}</option>`
+            });
+        });
+});
+
+estado.addEventListener('change', () => {
+    cidade.innerHTML = '<option>-- Selecione --</option>';
+
+    fetch(urlMunicipios(estado.value))
+        .then(response => response.json())
+        .then(dados => {
+            dados.map(cadaResult => {
+                cidade.innerHTML += `<option value="${cadaResult.id}">${cadaResult.nome}</option>`
+            });
+        });
+});
